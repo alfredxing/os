2957a32e874c85e09090e9981010f4229b6b1a26
Alfred Xing <alfredxing@users.noreply.github.com>
2016-01-02 10:03:29 -0800

    C kernel

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -3,7 +3,9 @@ global long_mode_start
 section .text
 bits 64
 long_mode_start:
-	; Print `OKAY` to screen
-	mov rax, 0x022002690248
-	mov qword [0xb8000], rax
+	; Call kernel.c main
+	extern kernel_main
+	call kernel_main
+
+	cli
 	hlt
