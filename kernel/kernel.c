0c0e5eb774c619ecd189302d3ed801f9df1db3af
Alfred Xing <alfredxing@users.noreply.github.com>
2016-01-02 18:56:23 -0800

    `stdio` library

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 
 #include <kernel/tty.h>
 
@@ -8,7 +9,7 @@ void kernel_main() {
 	// Initialize terminal
 	terminal_initialize();
 
-	terminal_writestring("Hello world!\n\n"
-			"This is the 05 kernel.\n"
-			"It can't do anything yet except print!\n\n");
+	printf("Hello world!\n\n"
+		"This is the 05 kernel.\n"
+		"It can't do anything yet except print!\n");
 }
---
+++ libc/include/stdio.h
@@ -0,0 +1,8 @@
+#ifndef _STDIO_H
+#define _STDIO_H 1
+
+int printf(const char* __restrice, ...);
+int putchar(int);
+int puts(const char*);
+
+#endif
